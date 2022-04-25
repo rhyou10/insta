03ea422a3b0a339a51673a007134a462278a9100
rhyou10 <rhyou10@nate.com>
2022-04-25 23:28:12 +0900

    로그인 로그아웃 구

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
+    path('signup/', views.signup, name='signup'), ## setting.LOGIN_URL로 주소 설정되어있다.
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout')
     
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,27 @@
-from tokenize import Single
 from django.shortcuts import redirect, render
 from .forms import SignupForm
 from django.contrib import messages
+
+from django.contrib.auth.views import LoginView, logout_then_login
+from django.contrib.auth import login as auth_login #로그인처리
+
+login = LoginView.as_view(template_name="accounts/login_form.html")
+
+
+def logout(request):
+    messages.success(request, '로그아웃 성공하였습니다.')
+    return logout_then_login(request)
 
 def signup(request):
     if request.method=='POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            form.save()
+            signed_user = form.save()
+            auth_login(request, signed_user) #바로 로그인 처리
             messages.success(request, "회원가입 성공")
-            return redirect("root")
+
+            next_url = request.GET.get('next','/')
+            return redirect(next_url)
     else:
         form = SignupForm()
     return render(request, 'accounts/signup_form.html',{
